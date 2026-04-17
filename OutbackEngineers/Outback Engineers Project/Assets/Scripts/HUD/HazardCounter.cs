420df1a753d9e110424a85d25937947ee44ad2d8
Lochlan Dunne <ignobliss42@gmail.com>
2026-04-17 15:31:24 +1000

    Added most basic HUD Elements and a hazard counter/tracker.
    Added the first part of the basic HUD, focusing on the Hazard Counter. The Hazard Counter works by tracking every game object in the scene with the Hazard component that Miles created, though for it to work, I had to update the class to inherit from Monobehaviour scripts, storing it as an array and updating the linked HUD element with the array's current length.

---
+++ OutbackEngineers/Outback Engineers Project/Assets/Scripts/HUD/HazardCounter.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class HazardCounter : MonoBehaviour
+{
+
+    public TextMeshProUGUI hazardCounter;
+    Hazard[] hazards;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hazards = Object.FindObjectsByType<Hazard>(FindObjectsSortMode.None);
+        hazardCounter.text = "Number ofHazards: " + hazards.Length;
+    }
+}
