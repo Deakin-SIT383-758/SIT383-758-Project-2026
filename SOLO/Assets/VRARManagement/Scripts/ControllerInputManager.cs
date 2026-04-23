8e4294b2afc9f3d49bc9ebdbcd84f1bf0d9a46da
Mick M <mickwpmcdonnell@gmail.com>
2026-04-23 14:56:32 +0930

    feat: Add elevation offset to VR transition

--- SOLO/Assets/VRARManagement/Scripts/ControllerInputManager.cs
+++ SOLO/Assets/VRARManagement/Scripts/ControllerInputManager.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class ControllerInputManager : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
     [SerializeField] private OVRPassthroughLayer passthroughLayer;
-
+    [SerializeField] private float verticalOffset = 50f;
     private bool inVR = false;
 
     private void Start()
@@ -22,6 +23,13 @@ public class ControllerInputManager : MonoBehaviour
     {
         inVR = !inVR;
         passthroughLayer.textureOpacity = inVR ? 1 : 0;
+        float offset = inVR ? verticalOffset : -verticalOffset;
+        Vector3 targetPos = new Vector3(
+            playerTransform.position.x,
+            playerTransform.position.y + offset,
+            playerTransform.position.z);
+
+        playerTransform.position = targetPos;
     }
 
 }
