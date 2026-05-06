8e0afa2e230809ad1d34b082dc00e252739f46f9
MilesOG22 <milesco22@gmail.com>
2026-05-06 11:37:10 +1000

    chore: Project Validation fixes

--- OutbackEngineers/Outback Engineers Project/Assets/Scripts/RunwaySpawner.cs
+++ OutbackEngineers/Outback Engineers Project/Assets/Scripts/RunwaySpawner.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RunwayManager : MonoBehaviour
+public class RunwaySpawner : MonoBehaviour
 {
     [Header("Runway Prefabs")]
     public GameObject[] runwayPrefabs;
