5e76b090b3f61669fe825240422ba4e68b413a03
jaiwats <jaiwats@gmail.com>
2026-05-13 13:22:16 +1000

    Added the shake fully
    I added the shake into the simulation with all the haptics.

--- VoiceContorls/Assets/Scripts/passthru.cs
+++ VoiceContorls/Assets/Scripts/passthru.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Oculus.Haptics;
 using Meta.XR.MRUtilityKit;
 
 public class passthru : MonoBehaviour
@@ -10,9 +11,19 @@ public class passthru : MonoBehaviour
     public float rayLength = 5;
     public MRUKAnchor.SceneLabels lableFilter;
     public GameObject Stuckup;
+    [SerializeField] private Vector3 up = new Vector3(0, 0.5f, 0);
+    [SerializeField] private GameObject controller;
+
+    public HapticClip hapticClip;
+    float timer = 0.0f;
+    private HapticClipPlayer player;
+
+    public AudioSource Eudio;
+
 
     void Start()
     {
+        player = new HapticClipPlayer(hapticClip);
         if (passthroughLayer != null)
         {
             passthroughLayer.enabled = true;
@@ -26,7 +37,7 @@ public class passthru : MonoBehaviour
         {
             TogglePassthrough();
         }
-        if (OVRInput.GetDown(OVRInput.RawButton.B))
+        if (OVRInput.Get(OVRInput.RawButton.B))
         {
             Ray ray = new Ray(rayStartpoint.position, rayStartpoint.forward);
             MRUKRoom room = MRUK.Instance.GetCurrentRoom();
@@ -36,11 +47,31 @@ public class passthru : MonoBehaviour
             if (hasHit)
             {
                 Vector3 hitpoint = hit.point;
-                Vector3 hitNormal = hit.normal;
+                Vector3 hitNormal = controller.transform.localEulerAngles;
 
-                Stuckup.transform.position = hitpoint;
-                Stuckup.transform.rotation = Quaternion.LookRotation(hitNormal);
+
+                Stuckup.transform.position = hitpoint + up;
+
+                //Change rotation
+                Vector3 newRotation = Stuckup.transform.eulerAngles;
+                newRotation.y = controller.transform.eulerAngles.y;
+                Stuckup.transform.eulerAngles = newRotation;
+
+                timer -= Time.deltaTime;
+                if (timer < 0.0f)
+                {
+                    player.Play(Controller.Left);
+                    player.Play(Controller.Right);
+                    timer = player.clipDuration;
+                    Eudio.Play();
+                }
             }
+        }
+
+        if (OVRInput.GetUp(OVRInput.RawButton.B))
+        {
+            timer = player.clipDuration;
+            Eudio.Stop();
         }
     }
     private void TogglePassthrough()
